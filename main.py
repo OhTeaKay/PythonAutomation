eb17a1ebe4518ef16976d69f7ac1655977ed12b0
Oskar <oskarmail@onet.pl>
2023-02-05 18:30:05 +0100

    ...

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+website = "https://www.thesun.co.uk/sport/football/"
+path = "/Users/Oskar/Desktop/chromedriver_win32/chromedriver"
 
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+driver.get(website)
+
+containers = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
+
+for container in containers:
+    title = container.find_element(by="xpath", value='./a/h3').text
+    subtitle = container.find_element(by="xpath", value='./a/p').text
+    link = container.find_element(by="xpath", value='./a').get_attribute("href")
+
+input("Press Enter to close the browser")
+driver.quit()
