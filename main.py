a033f4dbd2da12e6595ebb00e5ee6f5b3abdab56
Oskar <oskarmail@onet.pl>
2023-02-06 19:32:09 +0100

    added dictionary to contain page fragments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import pandas as pd
 
 website = "https://www.thesun.co.uk/sport/football/"
 path = "/Users/Oskar/Desktop/chromedriver_win32/chromedriver"
@@ -10,10 +11,22 @@ driver.get(website)
 
 containers = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
 
+titles = []
+subtitles = []
+links = []
+
+
 for container in containers:
     title = container.find_element(by="xpath", value='./a/h3').text
     subtitle = container.find_element(by="xpath", value='./a/p').text
     link = container.find_element(by="xpath", value='./a').get_attribute("href")
+    titles.append(title)
+    subtitles.append(subtitle)
+    link.append(link)
+
+article_dictionary = {'title': titles, 'subtitle': subtitles, 'link': links}
+
+pd.DataFrame()
 
 input("Press Enter to close the browser!")
 driver.quit()
